9272e8ee5a9b00e9ae9b75d11fbab9e0c6fcc545
Christoph Müller <iblzm@hotmail.de>
2016-09-25 16:48:43 +0200

    +UMD

--- oneTimeEvent.es5.js
+++ oneTimeEvent.es5.js
@@ -1,115 +1,148 @@
-'use strict';
+(function (global, factory) {
+  if (typeof define === "function" && define.amd) {
+    define(['exports'], factory);
+  } else if (typeof exports !== "undefined") {
+    factory(exports);
+  } else {
+    var mod = {
+      exports: {}
+    };
+    factory(mod.exports);
+    global.oneTimeEvent = mod.exports;
+  }
+})(this, function (exports) {
+  'use strict';
 
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
+  Object.defineProperty(exports, "__esModule", {
+    value: true
+  });
+  exports.newOneTimeEvent = newOneTimeEvent;
 
-var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
-
-exports.newOneTimeEvent = newOneTimeEvent;
-
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-var OneTimeEventPub = function () {
-  function OneTimeEventPub(priv) {
-    _classCallCheck(this, OneTimeEventPub);
-
-    this._priv = priv;
+  function _classCallCheck(instance, Constructor) {
+    if (!(instance instanceof Constructor)) {
+      throw new TypeError("Cannot call a class as a function");
+    }
   }
 
-  _createClass(OneTimeEventPub, [{
-    key: 'addHandler',
-    value: function addHandler(handler) {
-      this._priv.addHandler(handler);
+  var _createClass = function () {
+    function defineProperties(target, props) {
+      for (var i = 0; i < props.length; i++) {
+        var descriptor = props[i];
+        descriptor.enumerable = descriptor.enumerable || false;
+        descriptor.configurable = true;
+        if ("value" in descriptor) descriptor.writable = true;
+        Object.defineProperty(target, descriptor.key, descriptor);
+      }
     }
-  }, {
-    key: 'removeHandler',
-    value: function removeHandler(handler) {
-      this._priv.removeHandler(handler);
+
+    return function (Constructor, protoProps, staticProps) {
+      if (protoProps) defineProperties(Constructor.prototype, protoProps);
+      if (staticProps) defineProperties(Constructor, staticProps);
+      return Constructor;
+    };
+  }();
+
+  var OneTimeEventPub = function () {
+    function OneTimeEventPub(priv) {
+      _classCallCheck(this, OneTimeEventPub);
+
+      this._priv = priv;
     }
-  }]);
 
-  return OneTimeEventPub;
-}();
+    _createClass(OneTimeEventPub, [{
+      key: 'addHandler',
+      value: function addHandler(handler) {
+        this._priv.addHandler(handler);
+      }
+    }, {
+      key: 'removeHandler',
+      value: function removeHandler(handler) {
+        this._priv.removeHandler(handler);
+      }
+    }]);
 
-var OneTimeEvent = function () {
-  function OneTimeEvent(firstAdded, lastRemoved) {
-    _classCallCheck(this, OneTimeEvent);
+    return OneTimeEventPub;
+  }();
 
-    this._handlers = new Set();
-    this._firstAdded = firstAdded;
-    this._lastRemoved = lastRemoved;
-    this.pub = new OneTimeEventPub(this);
-  }
+  var OneTimeEvent = function () {
+    function OneTimeEvent(firstAdded, lastRemoved) {
+      _classCallCheck(this, OneTimeEvent);
 
-  _createClass(OneTimeEvent, [{
-    key: 'fire',
-    value: function fire() {
-      var currentHandlers = this._handlers;
       this._handlers = new Set();
-      var _iteratorNormalCompletion = true;
-      var _didIteratorError = false;
-      var _iteratorError = undefined;
+      this._firstAdded = firstAdded;
+      this._lastRemoved = lastRemoved;
+      this.pub = new OneTimeEventPub(this);
+    }
 
-      try {
-        for (var _iterator = currentHandlers[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
-          var handler = _step.value;
+    _createClass(OneTimeEvent, [{
+      key: 'fire',
+      value: function fire() {
+        var currentHandlers = this._handlers;
+        this._handlers = new Set();
+        var _iteratorNormalCompletion = true;
+        var _didIteratorError = false;
+        var _iteratorError = undefined;
 
-          handler.apply(undefined, arguments);
-        }
-      } catch (err) {
-        _didIteratorError = true;
-        _iteratorError = err;
-      } finally {
         try {
-          if (!_iteratorNormalCompletion && _iterator.return) {
-            _iterator.return();
+          for (var _iterator = currentHandlers[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+            var handler = _step.value;
+
+            handler.apply(undefined, arguments);
           }
+        } catch (err) {
+          _didIteratorError = true;
+          _iteratorError = err;
         } finally {
-          if (_didIteratorError) {
-            throw _iteratorError;
+          try {
+            if (!_iteratorNormalCompletion && _iterator.return) {
+              _iterator.return();
+            }
+          } finally {
+            if (_didIteratorError) {
+              throw _iteratorError;
+            }
           }
         }
       }
-    }
-  }, {
-    key: 'addHandler',
-    value: function addHandler(handler) {
-      if (!handler) {
-        throw 'handler must be a function.';
+    }, {
+      key: 'addHandler',
+      value: function addHandler(handler) {
+        if (!handler) {
+          throw 'handler must be a function.';
+        }
+        if (this._firstAdded && this._handlers.size === 0) {
+          this._handlers.add(handler);
+          this._firstAdded();
+        } else {
+          this._handlers.add(handler);
+        }
       }
-      if (this._firstAdded && this._handlers.size === 0) {
-        this._handlers.add(handler);
-        this._firstAdded();
-      } else {
-        this._handlers.add(handler);
+    }, {
+      key: 'removeHandler',
+      value: function removeHandler(handler) {
+        if (!handler) {
+          throw 'handler must be a function.';
+        }
+        this._handlers.delete(handler);
+        if (this._lastRemoved && this._handlers.size === 0) {
+          this._lastRemoved();
+        }
       }
-    }
-  }, {
-    key: 'removeHandler',
-    value: function removeHandler(handler) {
-      if (!handler) {
-        throw 'handler must be a function.';
+    }, {
+      key: 'isEmpty',
+      get: function get() {
+        return this._handlers.size === 0;
       }
-      this._handlers.delete(handler);
-      if (this._lastRemoved && this._handlers.size === 0) {
-        this._lastRemoved();
-      }
-    }
-  }, {
-    key: 'isEmpty',
-    get: function get() {
-      return this._handlers.size === 0;
-    }
-  }]);
+    }]);
 
-  return OneTimeEvent;
-}();
+    return OneTimeEvent;
+  }();
 
-function newOneTimeEvent() {
-  var firstAdded = arguments.length <= 0 || arguments[0] === undefined ? false : arguments[0];
-  var lastRemoved = arguments.length <= 1 || arguments[1] === undefined ? false : arguments[1];
+  function newOneTimeEvent() {
+    var firstAdded = arguments.length <= 0 || arguments[0] === undefined ? false : arguments[0];
+    var lastRemoved = arguments.length <= 1 || arguments[1] === undefined ? false : arguments[1];
 
-  return new OneTimeEvent(firstAdded, lastRemoved);
-}
+    return new OneTimeEvent(firstAdded, lastRemoved);
+  }
+});
 
--- test.js
+++ test.js
@@ -36,22 +36,22 @@ test('it works', (t) => {
   t.equals(handler0Passes, 1, 'Nr. 3');
   t.deepEquals(handler0Args, ['x', 'y'], 'Nr. 4');
 
-  t.equals(firstAddedPasses, 1, 'Nr. 4.5');
+  t.equals(firstAddedPasses, 1, 'Nr. 5');
   e.pub.addHandler(handler1);
-  t.equals(firstAddedPasses, 2, 'Nr. 4.8');
+  t.equals(firstAddedPasses, 2, 'Nr. 6');
   e.pub.addHandler(handler1);
   e.pub.addHandler(handler0);
   e.pub.addHandler(handler0);
   e.pub.removeHandler(handler1);
 
-  t.equals(lastRemovedPasses, 0, 'Nr. 14');
+  t.equals(lastRemovedPasses, 0, 'Nr. 7');
   e.pub.removeHandler(handler0);
-  t.equals(lastRemovedPasses, 1, 'Nr. 15');
+  t.equals(lastRemovedPasses, 1, 'Nr. 8');
 
   e.fire('A');
-  t.equals(handler1Passes, 0, 'Nr. 16');
-  t.equals(handler0Passes, 1, 'Nr. 17');
-  t.equals(firstAddedPasses, 2, 'Nr. 18');
-  t.equals(lastRemovedPasses, 1, 'Nr. 19');
+  t.equals(handler1Passes, 0, 'Nr. 9');
+  t.equals(handler0Passes, 1, 'Nr. 10');
+  t.equals(firstAddedPasses, 2, 'Nr. 11');
+  t.equals(lastRemovedPasses, 1, 'Nr. 12');
   t.end();
 });
